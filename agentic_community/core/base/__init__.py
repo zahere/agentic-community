9a19afccac3caa035073617acaf3f0cef4a20df1
Zaher Khateeb <zaher.master@gmail.com>
2025-05-23 12:03:07 +0300

    Add ToolConfig to base module exports

--- agentic_community/core/base/__init__.py
+++ agentic_community/core/base/__init__.py
@@ -1,6 +1,6 @@
 """Base module - Shared components between editions."""
 
 from .agent import BaseAgent, AgentConfig
-from .tool import BaseTool
+from .tool import BaseTool, ToolConfig
 
-__all__ = ["BaseAgent", "AgentConfig", "BaseTool"]
+__all__ = ["BaseAgent", "AgentConfig", "BaseTool", "ToolConfig"]
