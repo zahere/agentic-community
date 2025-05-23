1eb7d060856076e8df6d60e174155780d6dc7b8a
Zaher Khateeb <zaher.master@gmail.com>
2025-05-23 11:25:08 +0300

    Remove AgentConfig from base module exports

--- agentic_community/core/base/__init__.py
+++ agentic_community/core/base/__init__.py
@@ -1,6 +1,6 @@
 """Base module - Shared components between editions."""
 
-from .agent import BaseAgent, AgentConfig
+from .agent import BaseAgent
 from .tool import BaseTool, ToolConfig
 
-__all__ = ["BaseAgent", "AgentConfig", "BaseTool", "ToolConfig"]
+__all__ = ["BaseAgent", "BaseTool", "ToolConfig"]
